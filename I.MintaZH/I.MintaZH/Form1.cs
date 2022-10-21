59798de4a1141d75e159e17e36885beac6857a56
gerda.szalanczi <gerda.mylene.szalanczi@sap.com>
2022-10-21 13:53:57 +0200

    valami később befejezett commit-ja

--- I.MintaZH/I.MintaZH/Form1.cs
+++ I.MintaZH/I.MintaZH/Form1.cs
@@ -26,23 +26,7 @@ namespace I.MintaZH
         Excel.Workbook xlWB; //munkafüzet
         Excel.Worksheet xlSheet; // munkalap
 
-        private string GetCell(int x, int y)
-        {
-            string ExcelCoordinate = "";
-            int dividend = y;
-            int modulo;
-
-            while (dividend > 0)
-            {
-                modulo = (dividend - 1) % 26;
-                ExcelCoordinate = Convert.ToChar(65 + modulo).ToString() + ExcelCoordinate;
-                dividend = (int)((dividend - modulo) / 26);
-            }
-            ExcelCoordinate += x.ToString();
-
-            return ExcelCoordinate;
-        }
-
+        
         public Form1()
         {
             InitializeComponent();
