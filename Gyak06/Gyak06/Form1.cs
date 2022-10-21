acafc187f8e0b65df5af997cef083fe3927789d5
gerda.szalanczi <gerda.mylene.szalanczi@sap.com>
2022-10-21 14:47:24 +0200

    XML feldolgozás

--- Gyak06/Gyak06/Form1.cs
+++ Gyak06/Gyak06/Form1.cs
@@ -9,6 +9,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Xml;
 
 namespace Gyak06
 {
@@ -21,6 +22,11 @@ namespace Gyak06
         {
             InitializeComponent();
 
+            IkszEmEl(Lekerdezes());
+        }
+
+        public string Lekerdezes()
+        {
             GetExchangeRatesRequestBody request = new GetExchangeRatesRequestBody();
             request.currencyNames = "EUR";
             request.startDate = "2020-01-01";
@@ -29,6 +35,28 @@ namespace Gyak06
             var response = mnbService.GetExchangeRates(request);
 
             var result = response.GetExchangeRatesResult;
+            return result;
+        }
+
+        private void IkszEmEl(string result) //majd a lekérdezés által visszaadott értéket hívjuk be a konstruktorból
+        {
+            XmlDocument xml = new XmlDocument();
+
+            xml.LoadXml(result); //ez az az érték, lekérdezés visszaadott értéke
+
+            //mnb exchange rates fő eleméből(lekérdezett napok) documentelement lekérdezése
+            foreach (XmlElement item in xml.DocumentElement) //speciális listából kérdezünk le, var nem elég ide
+            {
+                RateDate rateDate = new RateDate();
+                rateDate.Date =Convert.ToDateTime(item.GetAttribute("date"));
+                rateDate.Currency = ((XmlElement)item.ChildNodes[0]).GetAttribute("curr");
+                //1 vagy 100 egységnyi valuta értékét nézzük meg Ft-ban
+                var unit = Convert.ToInt16(((XmlElement)item.ChildNodes[0]).GetAttribute("unit"));
+                var value = Decimal.Parse(((XmlElement)item.ChildNodes[0]).InnerText);
+                if (unit != 0) rateDate.Value = value / unit;
+                Rates.Add(rateDate);
+            }
+            
         }
 
         private void Form1_Load(object sender, EventArgs e)
