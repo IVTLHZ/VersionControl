f251f3d6b52d4d934d1b3a5baf8d8378837fb3ce
gerda.szalanczi <gerda.mylene.szalanczi@sap.com>
2022-11-04 20:35:55 +0100

    labdagyár

---
+++ Gyak07/Gyak07/Entities/BallFactory.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Gyak07.Entities
+{
+    internal class BallFactory
+    {
+        public Ball CreateNew() //ball visszatérési érték
+        {
+            return new Ball(); //és létre is kell hozni az új labdát
+        }
+            }
+}
