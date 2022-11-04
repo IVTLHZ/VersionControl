3053ab640925a85313e81c8912e1996d548597a5
gerda.szalanczi <gerda.mylene.szalanczi@sap.com>
2022-11-04 23:50:45 +0100

    timeres

--- Gyak07/Gyak07/Form1.Designer.cs
+++ Gyak07/Gyak07/Form1.Designer.cs
@@ -29,12 +29,48 @@
         private void InitializeComponent()
         {
             this.components = new System.ComponentModel.Container();
+            this.mainPanel = new System.Windows.Forms.Panel();
+            this.createTimer = new System.Windows.Forms.Timer(this.components);
+            this.conveyorTimer = new System.Windows.Forms.Timer(this.components);
+            this.SuspendLayout();
+            // 
+            // mainPanel
+            // 
+            this.mainPanel.Location = new System.Drawing.Point(0, 0);
+            this.mainPanel.Name = "mainPanel";
+            this.mainPanel.Size = new System.Drawing.Size(822, 506);
+            this.mainPanel.TabIndex = 0;
+            // 
+            // createTimer
+            // 
+            this.createTimer.Enabled = true;
+            this.createTimer.Interval = 3000;
+            this.createTimer.Tick += new System.EventHandler(this.createTimer_Tick);
+            // 
+            // conveyorTimer
+            // 
+            this.conveyorTimer.Enabled = true;
+            this.conveyorTimer.Interval = 10;
+            this.conveyorTimer.Tick += new System.EventHandler(this.conveyorTimer_Tick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.mainPanel);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Panel mainPanel;
+        private System.Windows.Forms.Timer createTimer;
+        private System.Windows.Forms.Timer conveyorTimer;
     }
 }
 
--- Gyak07/Gyak07/Form1.cs
+++ Gyak07/Gyak07/Form1.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Gyak07.Entities;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -12,9 +13,38 @@ namespace Gyak07
 {
     public partial class Form1 : Form
     {
+        List<Ball> _balls = new List<Ball>();
+
+        //kifejtett prop
+        private BallFactory _factory;
+
+        public BallFactory Factory
+        {
+            get { return _factory; }
+            set { _factory = value; }
+        }
+
+
         public Form1()
         {
             InitializeComponent();
+            //példány feltöltése konstruktorból
+            Factory = new BallFactory();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void createTimer_Tick(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void conveyorTimer_Tick(object sender, EventArgs e)
+        {
+
         }
     }
 }
