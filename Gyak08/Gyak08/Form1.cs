93832704496a62bf5c89cbb1a1d3d0eac432599b
gerda.szalanczi <gerda.mylene.szalanczi@sap.com>
2022-11-11 14:07:29 +0100

    adatbetöltés

--- Gyak08/Gyak08/Form1.cs
+++ Gyak08/Gyak08/Form1.cs
@@ -12,9 +12,17 @@ namespace Gyak08
 {
     public partial class Form1 : Form
     {
+        List<Tick> ticks = new List<Tick>();
+        //ORM objektum példányosítása
+        PortfolioEntities context = new PortfolioEntities();
+
         public Form1()
         {
             InitializeComponent();
+            //adatábla (context.ticks) memóriába (tick típusú elemű listába)másolása
+            ticks = context.Ticks.ToList(); //tolist nélkül sql mondatok generálása
+            //DG feltöltés
+            dataGridView1.DataSource = ticks;
         }
 
         private void Form1_Load(object sender, EventArgs e)
