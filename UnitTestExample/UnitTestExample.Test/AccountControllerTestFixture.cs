c62d51550fba10933466411a8e33cdb29a8c423e
ivtlhz <ivtlhz@SZA0032042>
2022-11-25 14:34:46 +0100

    tezstkifejtés

--- UnitTestExample/UnitTestExample.Test/AccountControllerTestFixture.cs
+++ UnitTestExample/UnitTestExample.Test/AccountControllerTestFixture.cs
@@ -4,6 +4,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using UnitTestExample.Controllers;
 
 namespace UnitTestExample.Test
 {
@@ -12,7 +13,16 @@ namespace UnitTestExample.Test
         [Test]//fv attribútuma
         public void TestValidateEmail(string email, bool expectedResult) //bemenő paraméteres fv., public kell!
         {
-            
+            //ARRANGE
+            //kell add reference, project, és a unittestexample-s kell
+            //hogy ez sikerüljön:
+            var aC = new AccountController();
+            //ACT: bemenő email paraméter értékével meghívás, változóba tárolás
+            var actualResult = aC.ValidateEmail(email);
+            //ASSERT: azAssert osztály statikus AreEqual függvényével hasonlítsd össze a tényleges eredményt a bemeneti paraméterként kapott elvárt eredménnyel (expectedResult)
+            Assert.AreEqual(expectedResult, actualResult);
+            //több assert hívásnál akkor lesz sikeres a teszt, ha minden egyes hívás eredménye sikeres 
+
         }
     }
 }
