858b511c3183409005312177b17f8e89ee98b9c8
ivtlhz <ivtlhz@SZA0032042>
2022-11-25 14:29:35 +0100

    validáló fv

---
+++ UnitTestExample/UnitTestExample.Test/AccountControllerTestFixture.cs
@@ -0,0 +1,18 @@
+﻿using NUnit.Framework; //kell az attribútumhoz
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UnitTestExample.Test
+{
+    public class AccountControllerTestFixture
+    {   
+        [Test]//fv attribútuma
+        public void TestValidateEmail(string email, bool expectedResult) //bemenő paraméteres fv., public kell!
+        {
+            
+        }
+    }
+}
---
+++ UnitTestExample/UnitTestExample.Test/UnitTest1.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+namespace UnitTestExample.Test
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            
+        }
+
+        
+    }
+}
