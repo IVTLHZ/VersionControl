d82d0f05e355242c04fe8e9a86ccda5b3bf740d6
gerda.szalanczi <gerda.mylene.szalanczi@sap.com>
2022-12-09 14:20:16 +0100

    iteráció

--- IVTLHZ13/IVTLHZ13/Form1.Designer.cs
+++ IVTLHZ13/IVTLHZ13/Form1.Designer.cs
@@ -28,21 +28,35 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.label1 = new System.Windows.Forms.Label();
             this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(737, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(51, 20);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "label1";
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label1);
             this.Name = "Form1";
             this.Text = "Form1";
             this.Load += new System.EventHandler(this.Form1_Load);
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private System.Windows.Forms.Label label1;
     }
 }
 
--- IVTLHZ13/IVTLHZ13/Form1.cs
+++ IVTLHZ13/IVTLHZ13/Form1.cs
@@ -4,6 +4,7 @@ using System.ComponentModel;
 using System.Data;
 using System.Drawing;
 using System.Linq;
+using System.Reflection.Emit;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
@@ -35,13 +36,31 @@ namespace IVTLHZ13
             //gc.Start(true); //játkindítás, true miatt az utoljára hozzáadott játékos vezérelhető lesz
             //wasd billentyűkkel irányítás
 
+            gc.GameOver += Gc_GameOver;
+
+            //ha csak ez van, akkor nem túl fejlett a populáció
             for (int i = 0; i < populationSize; i++)
             {
                 gc.AddPlayer(nbrOfSteps); //a gép által vezérelt játokosnak megadjuk max hányat léphet
                 //véletlenszerű indulás, 10 lépés után megállás
             }
 
+            
             gc.Start(); //nem kell true, mert mindenkit a gép vezérel
+        }
+
+        private void Gc_GameOver(object sender)
+        {
+            generation++;
+            label1.Text = string.Format(
+                "{0}. generáció",
+                generation);
+
+
+            var playerList = from p in gc.GetCurrentPlayers() //sorbarendezés rátermettség alapján
+                             orderby p.GetFitness() descending //getfittness double érték, attól függ milyen közel jutott a játékos a célhoz
+                             select p;
+            var topPerformers = playerList.Take(populationSize / 2).ToList(); //referencia típusú változó az előbbi lekérdezés, de nme ürül le a lista újraindítással ,új lista generálódik, nem baj, hogy az eredeti lista kiürül
         }
 
         private void Form1_Load(object sender, EventArgs e)
