cc1e75ff8b612ee72ec4f7f33e4059c0764fd31a
gerda.szalanczi <gerda.mylene.szalanczi@sap.com>
2022-12-09 13:59:15 +0100

    branch + projekt

---
+++ IVTLHZ11/IVTLHZ11/Form1.cs
@@ -0,0 +1,197 @@
+﻿using IVTLHZ11.Entities;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace IVTLHZ11
+{
+    public partial class Form1 : Form
+    {
+        Random rng = new Random(1234); //matematikai képlet segítségével előállít egy olyan számsorozatot, amiben a lehetséges értékek véletlen sorrendben egyenletes eloszlással követik egymást. Ez azt jelenti, hogy a számsorozat tetszőleges pontjától, egy forrástól (Seed) elindulva a sorban következő értékek lényegében véletlen számok lesznek
+        //seed órajel alapján / mi is deklarálhatjuk
+
+        List<Person> Population = new List<Person>();
+        List<BirthProbability> BirthProbabilities = new List<BirthProbability>();
+        List<DeathProbability> DeathProbabilities = new List<DeathProbability>();
+
+        List<int> Ffi = new List<int>();
+        List<int> No = new List<int>();
+
+        public Form1()
+        {
+            InitializeComponent();
+           
+           
+        }
+
+        private void Simulation()
+        {
+            richTextBox1.Text = "";
+
+            Population = GetPopulation(textBox1.Text);
+            BirthProbabilities = GetBirthProbabilities(@"C:\Temp\születés.csv");
+            DeathProbabilities = GetDeathProbabilities(@"C:\Temp\halál.csv");
+
+        
+            // Végigmegyünk a vizsgált éveken
+            for (int year = 2005; year <= numericUpDown1.Value; year++) //2005től vizsgálunk csak
+            {
+                // Végigmegyünk az összes személyen
+                for (int i = 0; i < Population.Count; i++)
+                {
+                    SimStep(Population[i], year);
+                }
+
+                int nbrOfMales = (from x in Population
+                                  where x.Gender == Gender.Male && x.IsAlive
+                                  select x).Count();
+                int nbrOfFemales = (from x in Population
+                                    where x.Gender == Gender.Female && x.IsAlive
+                                    select x).Count();
+                Console.WriteLine(//értékek konzolra iratása
+                    string.Format("Év:{0} Fiúk:{1} Lányok:{2}", year, nbrOfMales, nbrOfFemales));
+                
+                Ffi.Add(nbrOfMales);
+                No.Add(nbrOfFemales);
+
+                DisplayResults();
+            }
+            
+        }
+
+        private void SimStep(Person person, int year)
+        {
+            //Ha halott akkor kihagyjuk, ugrunk a ciklus következő lépésére
+            if (!person.IsAlive) return;
+
+            // Letároljuk az életkort, hogy ne kelljen mindenhol újraszámolni
+            byte age = (byte)(year - person.BirthYear);
+
+            // Halál kezelése
+            // Halálozási valószínűség kikeresése
+            double pDeath = (from x in DeathProbabilities
+                             where x.Gender == person.Gender && x.Age == age
+                             select x.DP).FirstOrDefault();
+            // Meghal a személy?
+            if (rng.NextDouble() <= pDeath)
+                person.IsAlive = false;
+
+            //Születés kezelése - csak az élő nők szülnek
+            if (person.IsAlive && person.Gender == Gender.Female)
+            {
+                //Szülési valószínűség kikeresése
+                double pBirth = (from x in BirthProbabilities
+                                 where x.Age == age
+                                 select x.BP).FirstOrDefault();
+                //Születik gyermek?
+                if (rng.NextDouble() <= pBirth)
+                {
+                    Person újszülött = new Person();
+                    újszülött.BirthYear = year;
+                    újszülött.NbrOfChildren = 0;
+                    újszülött.Gender = (Gender)(rng.Next(1, 3)); //véletlenszerű nem, a felső korlát nem megengedő
+                    Population.Add(újszülött);
+                }
+            }
+
+        }
+
+
+        private List<DeathProbability> GetDeathProbabilities(string v)
+        {
+            List<DeathProbability> deathProbabilities = new List<DeathProbability>();
+
+            using (StreamReader sr = new StreamReader(v, Encoding.Default)) //olvasásra megnyitás
+            {
+                while (!sr.EndOfStream)
+                {
+                    var line = sr.ReadLine().Split(';'); //iteratívan elemekre bontás tulajdonságok miatt
+                    deathProbabilities.Add(new DeathProbability() //megfelelő objektum képzése és listához adása
+                    {
+                        Gender = (Gender)Enum.Parse(typeof(Gender), line[0]),
+                        Age = int.Parse(line[1]),
+                        DP = double.Parse(line[2])
+                    });
+                }
+            }
+
+            return deathProbabilities; //adott listát ad vissza
+        }
+
+        private List<BirthProbability> GetBirthProbabilities(string v)
+        {
+            List<BirthProbability> birthProbabilities = new List<BirthProbability>();
+
+            using (StreamReader sr = new StreamReader(v, Encoding.Default)) //olvasásra megnyitás
+            {
+                while (!sr.EndOfStream)
+                {
+                    var line = sr.ReadLine().Split(';'); //iteratívan elemekre bontás tulajdonságok miatt
+                    birthProbabilities.Add(new BirthProbability() //megfelelő objektum képzése és listához adása
+                    {
+                        Age = int.Parse(line[0]),
+                        NbrOfChildren = int.Parse(line[1]),
+                        BP = double.Parse(line[2])
+                    });
+                }
+            }
+
+            return birthProbabilities; //adott listát ad vissza
+        }
+
+        private List<Person> GetPopulation(string csvpath)
+        {
+            List<Person> population = new List<Person>();
+
+            using (StreamReader sr = new StreamReader(csvpath, Encoding.Default)) //olvasásra megnyitás
+            {
+                while (!sr.EndOfStream)
+                {
+                    var line = sr.ReadLine().Split(';'); //iteratívan elemekre bontás tulajdonságok miatt
+                    population.Add(new Person() //megfelelő objektum képzése és listához adása
+                    {
+                        BirthYear = int.Parse(line[0]),
+                        Gender = (Gender)Enum.Parse(typeof(Gender), line[1]),
+                        NbrOfChildren = int.Parse(line[2])
+                    });
+                }
+            }
+
+            return population; //adott listát ad vissza
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Simulation();
+            
+        }
+
+        private void DisplayResults()
+        {
+            int i = 0;
+            for (int year = 2005; year <= numericUpDown1.Value; year++) //2005től vizsgálunk csak
+            {
+                richTextBox1.Text = String.Format("Szimulációs év: {0}\n\tFiúk: {1}\n\tLányok: {2}\n\n", year.ToString(), Ffi[i].ToString(), No[i].ToString()[i]);
+                i++;
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog ofd = new OpenFileDialog();
+            if (ofd.ShowDialog() == DialogResult.OK)
+            {
+                textBox1.Text = ofd.FileName;
+            }
+        }
+    }
+}
+
+
--- RPA02/RPA02/Program.cs
+++ RPA02/RPA02/Program.cs
@@ -1,39 +1,193 @@
-﻿using System;
+﻿using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Interactions;
+using OpenQA.Selenium;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
+using Excel = Microsoft.Office.Interop.Excel;
+using System.Reflection;
+using Microsoft.Office.Interop.Excel;
+using Actions = OpenQA.Selenium.Interactions.Actions;
+using System.IO;
 
 namespace RPA02
 {
-    internal class Program
+    class Program
     {
         static void Main(string[] args)
         {
+            //https://www.expedia.com/Flights-Search?flight-type=on&mode=search&trip=oneway&leg1=from%3Abudapest%2Cto%3Abarcelona%2Cdeparture%3A12%2F9%2F2022TANYT&options=cabinclass%3Aeconomy&passengers=children%3A0%2Cadults%3A1%2Cseniors%3A0%2Cinfantinlap%3AY&fromDate=12%2F9%2F2022&d1=2022-12-09
+            string url= "https://www.expedia.com/?pwaLob=wizard-flight-pwa";
+            string dep_place ="budapest";
+            string arr_place= "barcelona";
+            string dep_date= "Dec162022"; //csak ebben a formátumban működik (hónap nevének első 3 betűje, nap(2 karakter), év(2 karakter))
+            List<string[]> data = new List<string[]>();
 
-            //code-first megközelítés
+            char[] dep_date_chars = dep_date.ToCharArray();
+            dep_date = dep_date_chars[0].ToString() + dep_date_chars[1].ToString() + dep_date_chars[2].ToString() + " " + dep_date_chars[3] + dep_date_chars[4] + ", " + dep_date_chars[5] + dep_date_chars[6] + dep_date_chars[7] + dep_date_chars[8];
 
-            //connection string, ez látszik, hogy létrehozott egy local hostot, ebben master db-t
-            //Server=localhost\SQLEXPRESS;Database=master;Trusted_Connection=True;
-            //csak a sqlexpress nevet kell megadni majd ssms-ben
+            IWebDriver driver = new ChromeDriver();
+            driver.Url = url;
 
-            //adatbázist a robot elvárt funkcionalitásai alapján tervezünk
-            //futás során kell kívülről jövő paraméter, amit belerakunk
-            //pl összes létező hirdetést töltse le és lehet tovább nézni, hogy lett-e x idő óta új, változozz-e valami
+            driver.FindElement(By.XPath("//*[@id=\"uitk-tabs-button-container\"]/div[1]/li[2]/a")).Click();
 
-            //kézzel ssms-ben adatbázis és tábla létrehozása
-            //csatlakozás
-            //new database
-            //new table
+            driver.FindElement(By.ClassName("uitk-form-field-trigger")).Click();
+            driver.FindElement(By.Id("location-field-leg1-origin")).SendKeys(dep_place);
+            driver.FindElement(By.Id("location-field-leg1-origin")).SendKeys(Keys.Enter);
 
-            //telefonok lekérdezésére
-            //id-nál set primary key és is identity yes
-            //new item ADO.NET
-            //queryket kezelő osztály kell
-            DBManager.AddPhone(); //így adatb-ba kerül az osztályban megadott elem
+            driver.FindElement(By.CssSelector("[aria-label='Going to']")).Click();
+            driver.FindElement(By.Id("location-field-leg1-destination")).SendKeys(arr_place);
+            driver.FindElement(By.Id("location-field-leg1-destination")).SendKeys(Keys.Enter);
 
-            string name = DBManager.GetPhoneNameById(1);
-            Console.WriteLine(name);
+            driver.FindElement(By.Id("d1-btn")).Click();
+            driver.FindElement(By.CssSelector("[aria-label='" + dep_date + "']")).Click();
+
+            driver.FindElement(By.CssSelector("[data-stid='apply-date-picker']")).Click();
+
+            driver.FindElement(By.CssSelector("[data-testid='submit-button']")).Click();
+
+            while (true)
+            {
+                try
+                {
+                    string a = driver.FindElement(By.XPath("/html/body/div[2]/div[1]/div/div[2]/div[3]/div/section/main/ul/li[2]/div/div/div[1]/button/span")).Text;
+                    data.Add(textToData(a));
+                    break;
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine(e);
+                    Thread.Sleep(500);
+                }
+            }
+
+            IWebElement b;
+            for (int i = 3; i < 12; i++)
+            {
+                b = driver.FindElement(By.XPath("/html/body/div[2]/div[1]/div/div[2]/div[3]/div/section/main/ul/li[" + i + "]/div/div/div[1]/button/span"));
+
+                Actions actions = new Actions(driver);
+                actions.MoveToElement(b);
+                actions.Perform();
+
+                data.Add(textToData(b.Text));
+            }
+
+            exportToExcel(data);
+
+            foreach (string[] i in data){
+                 foreach (string j in i) {
+                     Console.WriteLine(j);
+                 }
+             }
+        }
+
+        static string[] textToData(string text)
+        {
+            string[] data = text.Split(',');
+            //rep tarsasag: 0
+            //dep time: 1
+            //arr time: 2
+            //price: 3
+            //remaining: 4
+            //Layover: 5
+            data[0] = data[0].Substring(37, (data[0].Length - 44));
+            data[1] = data[1].Substring(14, 7);
+            data[2] = data[2].Substring(13, 7);
+            if (data[data.Length - 1] == " Nonstop.")
+            {
+                if (data.Length == 6)
+                {
+                    data[3] = data[3].Substring(11, 5);
+                    data[4] = data[4].Substring(1, 1);
+                    data[5] = "Nonstop";
+                }
+                else
+                {
+                    data[3] = data[3].Substring(11, 5);
+                    data[4] = "Na";
+                    data = data.Append("Nonstop").ToArray();
+                }
+            }
+            else
+            {
+                if (data.Length == 7)
+                {
+                    data[3] = data[3].Substring(11, 5);
+                    data[4] = data[4].Substring(1, 1);
+                    data[5] = data[5].Substring(1, (data[5].Length - 6));
+                }
+                else
+                {
+                    data[3] = data[3].Substring(11, 5);
+                    data[5] = data[4].Substring(1, (data[4].Length - 6));
+                    data[4] = "Na";
+                }
+            }
+
+            return data;
+        }
+
+        static void exportToExcel(List<string[]> data)
+        {
+           
+            try
+            {
+                
+                Excel.Application xlApp = new Excel.Application();
+                
+                
+                if (xlApp!=null)
+                {
+                    Excel.Workbook xlWB = xlApp.Workbooks.Add();
+                    Excel.Worksheet xlWS = (Excel.Worksheet)xlWB.Sheets.Add();
+                    
+                    //headers
+                    xlWS.Cells[1, 1] = "Repülő társaság";
+                    xlWS.Cells[1, 2] = "Indulási idő";
+                    xlWS.Cells[1, 3] = "Érkezési idő";
+                    xlWS.Cells[1, 4] = "Ár";
+                    xlWS.Cells[1, 5] = "Elérhető jegyek";
+                    xlWS.Cells[1, 6] = "Átszállások száma";
+                    int line = 2;
+                    foreach (var item in data)
+                    {
+                        xlWS.Cells[line, 1] = item[0];
+                        xlWS.Cells[line, 2] = item[1];
+                        xlWS.Cells[line, 3] = item[2];
+                        xlWS.Cells[line, 4] = item[3];
+                        xlWS.Cells[line, 5] = item[4];
+                        xlWS.Cells[line, 6] = item[5];
+                        
+                        line++;
+                    }
+                    
+                    xlWS.get_Range("A1", "F1").Font.Bold = true;
+                    xlWS.get_Range("A1", "F1").VerticalAlignment =Excel.XlVAlign.xlVAlignCenter;
+                    
+                    xlWS.get_Range("A1", "F1").BorderAround2(Excel.XlLineStyle.xlContinuous, XlBorderWeight.xlThick);
+                    var workingDirectory = AppContext.BaseDirectory;
+                   
+                    xlApp.ActiveWorkbook.SaveAs(String.Concat(workingDirectory, "available_flights.xls"), Excel.XlFileFormat.xlWorkbookNormal);
+                    xlWB.Close();
+                    xlApp.Quit();
+                    System.Runtime.InteropServices.Marshal.FinalReleaseComObject(xlWS);
+                    System.Runtime.InteropServices.Marshal.FinalReleaseComObject(xlWB);
+                    System.Runtime.InteropServices.Marshal.FinalReleaseComObject(xlApp);
+                    GC.Collect();
+                    GC.WaitForPendingFinalizers();
+
+                }
+            }
+            catch (Exception ex)
+            {
+                string errMsg = string.Format("Error:{0}\nLine:{1}", ex.Message, ex.Source);
+                Console.WriteLine(errMsg);
+            }
+            
         }
     }
 }
